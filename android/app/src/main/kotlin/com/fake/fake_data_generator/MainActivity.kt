eb5bd082a0add425961b5ad8523c9671c1d9b195
Aatif <aatifdom10@gmail.com>
2023-08-24 01:16:17 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/fake/fake_data_generator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.fake.fake_data_generator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,3 @@
+class Constants {
+  static const url = 'https://random-data-api.com/api/v2/';
+}
